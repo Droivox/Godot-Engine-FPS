66da10e6743917f99f399525cd914c6cf4706c55
Guilherme Nascimento <brcontainer@yahoo.com.br>
2025-01-20 04:37:18 -0300

    Update rail.gd

--- Project/data/scripts/weapon/rail.gd
+++ Project/data/scripts/weapon/rail.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 	var mesh: MeshInstance = $mesh
 	var timer: Timer = $timer
 
-	mesh.translation.z = -mesh.mesh.mid_height/2;
-	timer.connect("timeout", self, "queue_free");
+	mesh.translation.z = -mesh.mesh.mid_height/2
+	timer.connect("timeout", self, "queue_free")
 
 
 func _process(delta: float) -> void:
-	translation -= (global_transform.basis.z * speed) * delta;
+	translation -= (global_transform.basis.z * speed) * delta
